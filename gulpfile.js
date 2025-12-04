959ef128ead777c8c0cdc2220635056e232e2a1e
Alexander Henrique <henriquealexander60@gmail.com>
2025-12-03 21:27:36 -0300

    Adicionanco imagens e imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('./dist/css'));
 }
-exports.default = styles;
+
+function comprimeImagem() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/imagens'));
+}
+
+exports.default = gulp.parallel(styles, comprimeImagem);
+
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
